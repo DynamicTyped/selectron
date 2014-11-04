a6cea125f6da31d6ebe639de9e7d46ad2f24c4a6
jcoutch <joseph.coutcher@gmail.com>
2014-11-04 16:24:42 -0500

    Fix "className" attribute

--- scripts/selectronTagItem.jsx
+++ scripts/selectronTagItem.jsx
@@ -15,7 +15,7 @@ var SelectronTagItem = React.createClass({
     this.props.toggleSelected(e.target.dataset.value);
   },
   render: function(){
-    return <button type="button" class="btn btn-primary" data-value={this.props.option.value} onClick={this.handleClick}>{this.props.option.text}</button>
+    return <button type="button" className="btn btn-primary" data-value={this.props.option.value} onClick={this.handleClick}>{this.props.option.text}</button>
   }
 });
 
